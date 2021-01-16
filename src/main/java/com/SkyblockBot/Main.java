d5b4719b795a57e7cc4797903055aab8b63b30d4
Krish Ranjan <krish45732@gmail.com>
2021-01-15 19:01:42 -0500

    Added lowest bin, player ah, and more

--- src/main/java/com/SkyblockBot/Main.java
+++ src/main/java/com/SkyblockBot/Main.java
@@ -8,8 +8,13 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.exceptions.RateLimitedException;
 
 import javax.security.auth.login.LoginException;
-import java.awt.*;
 import java.io.IOException;
+
+import com.SkyblockBot.Auction.*;
+import com.SkyblockBot.Slayer.*;
+import com.SkyblockBot.Guild.*;
+import com.SkyblockBot.Miscellaneous.*;
+import static com.SkyblockBot.Miscellaneous.BotUtils.*;
 
 public class Main {
         public static void main(String[] args)
@@ -18,26 +23,21 @@ public class Main {
                 CommandClientBuilder client = new CommandClientBuilder();
 
                 client.useDefaultGame();
-                client.setOwnerId("0280");
+                client.setOwnerId("385939031596466176");
                 client.setEmojis("✅", "⚠️", "❌");
                 client.useHelpBuilder(false);
                 client.setPrefix("!");
 
-                client.addCommands(
-                                new AboutCommand(new Color(9, 92, 13), "an all purpose skyblock bot",
-                                                new String[] { "Skyblock Slayer", "Skyblock Skill Average",
-                                                                "Skyblock Dungeons", "Skyblock Guild" }), // TODO: make
-                                                                                                          // custom
-                                                                                                          // about
-                                                                                                          // command
-                                new SlayerCommands(), // TODO: work on slayer commands
-                                new Help(waiter), new GuildCommands(waiter), // TODO: work on guild commands
-                                new AuctionCommands(), new AuctionCommandsAlias()
+                client.addCommands(new AboutCommand(), new SlayerCommands(), new Help(waiter),
+                                new GuildCommands(waiter), new AuctionCommands(), new AuctionCommandsAlias(),
+                                new BinCommands()
 
                 );
 
-                JDABuilder.createDefault(System.getenv("BOT_TOKEN"))// "Nzk2NzkxMTY3MzY2NTk0NTky.X_dDmQ.zOZSNLa6s1fBzyUD2jQtjOsX-a8")
-                                .setStatus(OnlineStatus.DO_NOT_DISTURB).setActivity(Activity.playing("Loading..."))
-                                .addEventListeners(waiter, client.build()).build();
+                setBotSettings();
+                JDABuilder.createDefault(botToken).setStatus(OnlineStatus.DO_NOT_DISTURB)
+                                .setActivity(Activity.playing("Loading...")).addEventListeners(waiter, client.build())
+                                .build();
         }
+
 }
