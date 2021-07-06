a40ab492276ae8507e711e4e337dcfbe34467074
Krish Ranjan <krish45732@gmail.com>
2021-07-06 19:54:03 -0400

    weight roles and custom prefix

--- Procfile
+++
@@ -1,1 +0,0 @@
-worker: java $JAVA_OPTS -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+UseStringDeduplication -jar build/libs/SkyblockPlus-0.0.1.jar
--- src/main/java/com/skyblockplus/utils/structs/HypixelKeyInformation.java
+++
@@ -1,9 +0,0 @@
-package com.skyblockplus.utils.structs;
-
-import java.util.concurrent.atomic.AtomicInteger;
-
-public class HypixelKeyInformation {
-
-	public final AtomicInteger remainingLimit = new AtomicInteger(120);
-	public final AtomicInteger timeTillReset = new AtomicInteger(0);
-}
