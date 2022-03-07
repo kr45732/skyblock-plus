860b545b714a1331dd6400e916e98ae8e990a670
kr45732 <52721908+kr45732@users.noreply.github.com>
2022-03-06 20:50:32 -0500

    sorry

--- src/main/java/com/skyblockplus/Main.java
+++ src/main/java/com/skyblockplus/Main.java
@@ -295,7 +295,7 @@ public class Main {
 		if (!transcriptDir.exists()) {
 			log.info((transcriptDir.mkdirs() ? "Successfully created" : "Failed to create") + " application transcript directory");
 		}
-		scheduler.schedule(System::gc, 30, TimeUnit.MINUTES); // Sorry for the war crimes
+		scheduler.schedule(System::gc, 20, TimeUnit.MINUTES); // Sorry for the war crimes
 	}
 
 	@PreDestroy
