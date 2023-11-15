6d5e4c6964a3133d7bf709ef73229668205f3b10
kr45732 <52721908+kr45732@users.noreply.github.com>
2023-11-14 22:12:35 -0500

    some comments

--- src/main/java/com/skyblockplus/api/controller/ApiController.java
+++ src/main/java/com/skyblockplus/api/controller/ApiController.java
@@ -39,6 +39,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Handles requests to api subdomain
+ */
 @RestController
 @RequestMapping(headers = "X-Subdomain-Internal=api")
 public class ApiController {
